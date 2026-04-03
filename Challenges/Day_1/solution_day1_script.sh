898e8303c92d4b17e3f0b3d75eee80a5ec9b2cab
os7 <os7@os7.tail9c2660.ts.net>
2026-04-03 13:07:43 +0500

    varibles

--- Challenges/Day_1/solution_day1_script.sh
+++ Challenges/Day_1/solution_day1_script.sh
@@ -1,31 +1,24 @@
 #!/bin/bash
+echo "hello everyone"
 
-# First line of the script is the shebang which tells the system how to execute
+name="ali"
+surname="shams"
 
-# Task  2: Echo
-echo "Scripting is fun with @TWS"
+num1=3
+num2=4
 
-# Task  3: Variables
-variable1="Hello"
-variable2="Bash"
+sum=$((num1 + num2))
 
-# Task  4: Using Variables
-greeting="$variable1, $variable2!"
-echo "$greeting Welcome to the world of Bash scripting!"
+echo "sum of $num1 and $num2 is: $sum"
 
-# Task  5: Using Built-in Variables
-echo "My current bash path - $BASH"
-echo "Bash version I am using - $BASH_VERSION"
-echo "PID of bash I am running - $$"
-echo "My home directory - $HOME"
-echo "Where am I currently? - $PWD"
-echo "My hostname - $HOSTNAME"
+cmd="ip addr show"
 
-# Task  6: Wildcards
-echo "Files with .txt extension in the current directory:"
+ip=$(hostname -I)
+echo "script name: $0"
+echo "first argumet: $1"
+echo "user name: $USER"
+echo "your ip addres is: $ip"
+echo "our ip is: $cmd"
+echo "lsiitng all txt files"
+
 ls *.txt
-
-
-
-#Make sure to provide execution permission with the following command:
-#chmod +x day1_script.sh
