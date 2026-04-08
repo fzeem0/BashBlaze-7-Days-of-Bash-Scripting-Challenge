7a063db3198016445bee21de765e5ff55e4a630f
os7 <os7@os7.tail9c2660.ts.net>
2026-04-08 10:14:20 +0500

    done

--- Challenges/Day_1/solution_day1_script.sh
+++ Challenges/Day_1/solution_day1_script.sh
@@ -11,14 +11,12 @@ sum=$((num1 + num2))
 
 echo "sum of $num1 and $num2 is: $sum"
 
-cmd="ip addr show"
 
 ip=$(hostname -I)
 echo "script name: $0"
 echo "first argumet: $1"
 echo "user name: $USER"
 echo "your ip addres is: $ip"
-echo "our ip is: $cmd"
 echo "lsiitng all txt files"
 
 ls *.txt
